3e5f534bb7fcda68e0c95ca75da57a2a5e3e0899
fermersky <forte201253@gmail.com>
2018-10-01 20:32:08 +0300

    init

---
+++ Sysprog/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Sysprog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(31, 28);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(534, 367);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(698, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "processes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 502);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Sysprog/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Sysprog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //foreach (var Process in Process.GetProcessesByName("atom"))
+            //{
+            //    //Console.WriteLine($"{Process.Id} {Process.ProcessName}");
+            //    Process.CloseMainWindow();
+            //    Process.Close();
+            //}
+            //Console.WriteLine();
+
+            Process proc = Process.GetProcessesByName("devenv")[0];
+
+            // var procThread = proc.Threads;
+            //foreach (ProcessThread thread in proc.Threads)
+            //{
+            //    Console.WriteLine($"id: {thread.Id}, time: {thread.StartTime}}");
+            //}
+
+           // Process proc2 = Process.Start("notepad++.exe", @"C:\Users\PKO\Desktop\index.txt");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView1.Columns.Clear();
+
+            listView1.Columns.Add("id");
+            listView1.Columns.Add("name");
+            listView1.View = View.Details;
+
+            foreach (Process proc in Process.GetProcesses())
+            {
+                ListViewItem item = new ListViewItem();
+                
+                item.Text = proc.Id.ToString();
+                item.SubItems.Add(proc.ProcessName);
+
+                listView1.Items.Add(item);
+            }
+        }
+    }
+}
